ff752c2e408e89ca0950e679dd63e834e4dbdb3b
paolodevprojects <paolo.dev.projects@gmail.com>
2024-02-19 17:09:04 -0500

    [ADD] forms with actions and loaders

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,14 @@
-import { Outlet, Link, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contact";
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { createContact, getContacts } from "../contacts";
 
 export async function loader() {
     const contacts = await getContacts();
     return { contacts };
+}
+
+export async function action() {
+    const contact = await createContact();
+    return { contact };
 }
 
 export default function Root() {
@@ -33,9 +38,9 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {contacts.length ? (
