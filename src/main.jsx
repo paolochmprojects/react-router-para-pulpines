86f538ca84640c48e28df52b2960fbe3221940ae
paolodevprojects <paolo.dev.projects@gmail.com>
2024-02-20 18:35:02 -0500

    [ADD] errorElemet for children routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
-import Contact, {loader as contactLoader} from './routes/contact'
+import Contact, { loader as contactLoader } from './routes/contact'
 import { action as destroyAction } from "./routes/destroy"
 import { action as contactAction } from './routes/contact'
-import {action as updateAction } from './routes/edit'
+import { action as updateAction } from './routes/edit'
 import EditContact from './routes/edit'
 import Index from './routes'
 
@@ -20,25 +20,30 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        index: true,
-        element: <Index/>,
-      },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: updateAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement:  <div>Oops! There was an error.</div>,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: updateAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          }
+        ]
       }
     ]
   },
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,6 +3,12 @@ import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
+    if (!contact) {
+        throw new Response("", {
+          status: 404,
+          statusText: "Not Found",
+        });
+      }
     return { contact };
 }
 
@@ -86,7 +92,7 @@ export default function Contact() {
 function Favorite({ contact }) {
     // yes, this is a `let` for later
     let favorite = contact.favorite;
-    
+
     const fetcher = useFetcher();
 
     if (fetcher.formData) {
